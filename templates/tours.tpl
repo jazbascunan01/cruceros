f69363574af751b360344ddd613e9d54657aa031
Jazmin Bascuñan <jazbascunan01@gmail.com>
2023-06-04 19:32:59 -0300

    Pagina tours terminada con el filtrado y estilos

--- templates/tours.tpl
+++ templates/tours.tpl
@@ -1,13 +1,14 @@
 {include file="header.tpl"}
-<form method="get" action="filtrar">
-    <label for="crucero">Seleccionar Crucero:</label>
-    <select name="crucero" id="crucero">
+<h2>Tours</h2>
+<form method="get" action="filtrar" class="formfilter">
+    <label class="labelfiltro" for="crucero">Seleccionar Crucero:</label>
+    <select class="selectfiltro" name="crucero" id="crucero">
         {foreach from=$cruceros item=crucero}
             <option value="{$crucero->ID}">{$crucero->nombre}</option>
         {/foreach}
     </select>
 
-    <button type="submit">Filtrar</button>
+    <button class="buttonfiltro" type="submit"><img class= "logo_nav" id= "btnfilter"src="images/6488674.png" alt="logo filtrar"></button>
 </form>
 <article class="recuadros">
 
--- templates_c/856ffabda0e0890873c6db670e0473c53c87aa80_0.file.tours.tpl.php
+++ templates_c/856ffabda0e0890873c6db670e0473c53c87aa80_0.file.tours.tpl.php
@@ -1,18 +1,18 @@
 <?php
-/* Smarty version 4.3.1, created on 2023-06-04 23:23:32
+/* Smarty version 4.3.1, created on 2023-06-05 00:30:20
   from 'C:\xampp\htdocs\ejemplos\jazmin\templates\tours.tpl' */
 
 /* @var Smarty_Internal_Template $_smarty_tpl */
 if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   'version' => '4.3.1',
-  'unifunc' => 'content_647d00d4181a16_85938698',
+  'unifunc' => 'content_647d107c6b7fe0_35126990',
   'has_nocache_code' => false,
   'file_dependency' => 
   array (
     '856ffabda0e0890873c6db670e0473c53c87aa80' => 
     array (
       0 => 'C:\\xampp\\htdocs\\ejemplos\\jazmin\\templates\\tours.tpl',
-      1 => 1685913500,
+      1 => 1685917815,
       2 => 'file',
     ),
   ),
@@ -22,12 +22,13 @@ if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
     'file:footer.tpl' => 1,
   ),
 ),false)) {
-function content_647d00d4181a16_85938698 (Smarty_Internal_Template $_smarty_tpl) {
+function content_647d107c6b7fe0_35126990 (Smarty_Internal_Template $_smarty_tpl) {
 $_smarty_tpl->_subTemplateRender("file:header.tpl", $_smarty_tpl->cache_id, $_smarty_tpl->compile_id, 0, $_smarty_tpl->cache_lifetime, array(), 0, false);
 ?>
-<form method="get" action="filtrar">
-    <label for="crucero">Seleccionar Crucero:</label>
-    <select name="crucero" id="crucero">
+<h2>Tours</h2>
+<form method="get" action="filtrar" class="formfilter">
+    <label class="labelfiltro" for="crucero">Seleccionar Crucero:</label>
+    <select class="selectfiltro" name="crucero" id="crucero">
         <?php
 $_from = $_smarty_tpl->smarty->ext->_foreach->init($_smarty_tpl, $_smarty_tpl->tpl_vars['cruceros']->value, 'crucero');
 $_smarty_tpl->tpl_vars['crucero']->do_else = true;
@@ -42,7 +43,7 @@ $_smarty_tpl->tpl_vars['crucero']->do_else = false;
 $_smarty_tpl->smarty->ext->_foreach->restore($_smarty_tpl, 1);?>
     </select>
 
-    <button type="submit">Filtrar</button>
+    <button class="buttonfiltro" type="submit"><img class= "logo_nav" id= "btnfilter"src="images/6488674.png" alt="logo filtrar"></button>
 </form>
 <article class="recuadros">
 
